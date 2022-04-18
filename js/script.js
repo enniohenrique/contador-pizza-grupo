218db802d18ac9abb6ccce85c97df4197a5323c0
enniohenrique <ennioh@icloud.com>
2022-04-17 21:13:49 -0400

    Update

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 function inicia() {
+
     document.getElementById("btn-add").addEventListener("click",function(){
         var lista=document.getElementById("listaElementos");
         var texto=document.getElementById("fnovo");
@@ -6,14 +7,30 @@ function inicia() {
         //if para verificar se o texto está vazio ou preenchido 
         if(texto.value != "") {
             var div=document.createElement("div");      /* Criando uma Div */
-            var ul=document.createElement("ul");        /* Criando uma ul */
-            var li=document.createElement("li");        /* Criando uma li */
+            var div2=document.createElement("div");      /* Criando uma Div */
+            var buttonAumentar=document.createElement("button");        /* Criando um botão */
+            var buttonDiminuir=document.createElement("button");        /* Criando um botão */
+            var pAumentar = "+";
+            var pDiminuir = "-";
+            var pDisplay = 0;
+
             div.setAttribute("class","elementos");     /* Atribuindo uma class para a div */
-            li.textContent=texto.value;     /* Salvando o nome que foi digitado na li */
+            div2.setAttribute("id","display");     /* Atribuindo uma class para a div2 */
+            buttonAumentar.setAttribute("class","btn-aumentar");     /* Atribuindo uma class para o botão */
+            buttonAumentar.setAttribute("onclick","soma()");
+            buttonDiminuir.setAttribute("class","btn-diminuir");     /* Atribuindo uma class para o botão */
+            buttonDiminuir.setAttribute("onclick","subtrai()"); 
+            div.textContent=texto.value;     /* Salvando o nome que foi digitado dentro da li */
+
             lista.appendChild(div);     /* Adicionando a div na lista de elementos do html */
             var cx=document.querySelector("#listaElementos div:last-child");        /* Salvando a  última div de class elementos na variavel cx */
-            cx.appendChild(ul);     /* Adicionando a ul dentro do conteudo da cx (A div de class elementos) */
-            ul.appendChild(li);     /* Adicionando a li dentro da ul */
+            div.appendChild(buttonDiminuir);  /* Adicionando o botão dentro da ul */
+            div.appendChild(div2);     /* Adicionando a div na lista de elementos do html */
+            div.appendChild(buttonAumentar);  /* Adicionando o botão dentro da ul */
+
+            buttonAumentar.textContent=pAumentar;       /* Colocando o texto no Botão */
+            buttonDiminuir.textContent=pDiminuir;       /* Colocando o texto no Botão */
+            div2.textContent=pDisplay;      /* Colocando o N° 0 dentro do display */
             texto.value="";     
             texto.focus();
         }
@@ -25,4 +42,23 @@ function inicia() {
     });
 }
 
+
+function soma() {
+    var display = document.getElementById('display').innerHTML; /* Pegando o valor que está dentro do id 'display do html */
+    document.getElementById('display').innerHTML = eval(display) + 1;   
+}
+
+function subtrai() {
+    /* Se o contador for maior que 0, pode diminui. Caso contrário, o valor vai continuar em 0, pois se não ficaria negativo */
+    if(document.getElementById('display').innerHTML > 0)
+        {
+        var display = document.getElementById('display').innerHTML;
+        document.getElementById('display').innerHTML = eval(display) - 1;
+    } 
+    else {
+        document.getElementById('display').innerHTML = "0";
+    }
+}
+
 window.addEventListener("load",inicia);
+
